6da28af7032ad7bbee31b7d3d6bb2a700ec6d047
DeenoDev <deyanoklaasen@outlook.com>
2023-08-15 23:55:15 +0200

    Signup form completed.

--- index.php
+++ index.php
@@ -8,6 +8,16 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+<h3>Signup</h3>
+
+<form action="includes/signup.inc.php" method="post">
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="pwd" placeholder="Password">
+    <input type="text" name="email" placeholder="E-mail">
+    <button>Signup</button>
+
+</form>
     
 </body>
 </html>
