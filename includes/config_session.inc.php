b061d55c72d344209884eeba423c239987cdd15c
DeenoDev <deyanoklaasen@outlook.com>
2023-08-16 23:55:06 +0200

    Created session cookie parameters.

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -2,3 +2,11 @@
 
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_strict_mode', 1);
+
+session_set_cookie_params([
+    'lifetime' => 1800,
+    'domain' => 'localhost',
+    'path' => '/',
+    'secure' => true,
+    'httponly' => true
+]);
