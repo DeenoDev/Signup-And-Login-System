80908ebf7d3f18eb720299524b6e5299df16bfbd
DeenoDev <deyanoklaasen@outlook.com>
2023-08-16 22:25:31 +0200

    Added bootstrap CDN.

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
     <input type="text" name="username" placeholder="Username">
     <input type="password" name="pwd" placeholder="Password">
     <button>Login</button>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
 </form>
 
 <h3>Signup</h3>
