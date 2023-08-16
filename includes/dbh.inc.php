52f00b2843f5d587ae73b5008d796a529382865b
DeenoDev <deyanoklaasen@outlook.com>
2023-08-16 23:28:18 +0200

    database connection script completed. Used Try & Catch function to setup up connection parameters.

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,3 +4,12 @@ $host = 'localhost';
 $dbname = 'signup-db';
 $dbusername = 'root';
 $dbpassword = '';
+
+try {
+    $pdo = newPDO("mysql:host=$host; dbname=$dbname", $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+
+}
