7a2f31ac250a62a904ece7f4f66361c8a89f296a
DeenoDev <deyanoklaasen@outlook.com>
2023-08-16 22:31:03 +0200

    Added some basic positional styling to form using bootstrap utillity classes.

--- index.php
+++ index.php
@@ -7,27 +7,29 @@
     <link rel="stylesheet" href="reset.css">
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body class="position-relative">
 
-<h3>Login</h3>
-
-<form action="includes/login.inc.php" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="pwd" placeholder="Password">
-    <button>Login</button>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
-</form>
-
-<h3>Signup</h3>
-
-<form action="includes/signup.inc.php" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="pwd" placeholder="Password">
-    <input type="text" name="email" placeholder="E-mail">
-    <button>Signup</button>
-
-</form>
+<div class="form-container position-absolute top-50 start-50 translate-middle">
+    <h3>Login</h3>
+    
+    <form action="includes/login.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <button>Login</button>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    </form>
+    
+    <h3>Signup</h3>
+    
+    <form action="includes/signup.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="E-mail">
+        <button>Signup</button>
+    
+    </form>
+</div>
     
 </body>
 </html>
