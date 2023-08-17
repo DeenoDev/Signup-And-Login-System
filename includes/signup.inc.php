64ab7c8ed6350f56b33c6d65acc08823030f1cb6
DeenoDev <deyanoklaasen@outlook.com>
2023-08-18 00:00:34 +0200

    Wrote script for signup logic and form validation.

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -8,6 +8,19 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
 
     try{
         require_once 'dbh.inc.php';
+        require_once 'signup_model.inc.php';
+        require_once 'signup_contr.inc.php';
+
+        //ERROR HANDLERS
+        if (is_input_empty($username, $pwd, $email)) {
+
+        }
+        if (is_email_invalid($email)) {
+
+        }
+
+        
+
 
     } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
--- includes/signup_contr.inc.php
+++ includes/signup_contr.inc.php
@@ -1,3 +1,21 @@
 <?php
 
 declare(strict_types=1);
+
+function is_input_empty($username, $pwd, $email) {
+    if (empty($username) || empty($pwd) || empty($email)) {
+        return true;
+    } else {
+        return false;
+    }
+     
+}
+
+function is_email_invalid($email) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
+     
+}
