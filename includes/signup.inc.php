837b586f611ae8fc966b8d87149a4d057ca9b636
DeenoDev <deyanoklaasen@outlook.com>
2023-08-17 23:37:58 +0200

    Created POST method variables.

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -2,6 +2,10 @@
 
 if($_SERVER["REQUEST_METHOD"] === "POST") {
 
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $email = $_POST["email"];
+
 } else {
     header("Location: ../index.php");
     die();
