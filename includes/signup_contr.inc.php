eac42b745354662efefa88c4b0fe6a28d96f279a
DeenoDev <deyanoklaasen@outlook.com>
2023-08-18 00:04:08 +0200

    declared string types within signup_contr.inc.php.

--- includes/signup_contr.inc.php
+++ includes/signup_contr.inc.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-function is_input_empty($username, $pwd, $email) {
+function is_input_empty(string $username, string $pwd, string $email) {
     if (empty($username) || empty($pwd) || empty($email)) {
         return true;
     } else {
@@ -11,8 +11,17 @@ function is_input_empty($username, $pwd, $email) {
      
 }
 
-function is_email_invalid($email) {
+function is_email_invalid(string $email) {
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
+     
+}
+
+function is_username_taken(string $username) {
+    if () {
         return true;
     } else {
         return false;
