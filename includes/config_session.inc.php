99f0066490ab558cd7aea56e5e000ae850522a66
DeenoDev <deyanoklaasen@outlook.com>
2023-08-17 23:31:27 +0200

    Refactored session regeneration script and completed config_session.inc.php file

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -13,7 +13,20 @@ session_set_cookie_params([
 
 session_start();
 
-if () {
-    session_regenerate_id();
-    $_SESSION["last_regeneration"] = time();
+if (isset($_SESSION["last_regeneration"])) {
+    function regenerate_session_id()
+} else {
+    $interval = 60 * 30;
+    if(time - $_SESSION["last_regeneration"] >= $interval){
+        session_regenerate_id();
+        $_SESSION["last_regeneration"] = time();
+
+    }
+}
+
+function regenerate_session_id() 
+{
+        session_regenerate_id();
+        $_SESSION["last_regeneration"] = time();
+
 }
