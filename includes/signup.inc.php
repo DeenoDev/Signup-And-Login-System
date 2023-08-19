52bf821fc88680e19453d0a87d8ee463905576cd
DeenoDev <deyanoklaasen@outlook.com>
2023-08-19 12:44:40 +0200

    Created an associative array for errpr handlers and added error messages for each error.

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -12,16 +12,22 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
         require_once 'signup_contr.inc.php';
 
         //ERROR HANDLERS
+        $errors = [];
+
         if (is_input_empty($username, $pwd, $email)) {
+            $errors["empty_input"] = "Fill in all fields!";
 
         }
         if (is_email_invalid($email)) {
+            $errors["invalid_email"] = "Invalid email used!";
 
         }
         if (is_username_taken($pdo, $username)) {
+            $errors["username_taken"] = "Username already taken!";
 
         }
         if (is_email_registered($pdo, $email)) {
+            $errors["email_used"] = "Email already registered!";
 
         }
 
