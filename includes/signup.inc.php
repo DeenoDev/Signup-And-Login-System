0295939bb5b141c007447995a4287e2960c70f43
DeenoDev <deyanoklaasen@outlook.com>
2023-08-19 12:39:07 +0200

    Added get_email function to signup.inc.php file.

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -21,7 +21,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
         if (is_username_taken($pdo, $username)) {
 
         }
-        if () {
+        if (is_email_registered($pdo, $email)) {
 
         }
 
