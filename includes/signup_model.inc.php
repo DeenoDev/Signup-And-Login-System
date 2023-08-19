d6a74c33fcc7e243cf1ae9a377cfad6e2488bbd7
DeenoDev <deyanoklaasen@outlook.com>
2023-08-19 12:19:23 +0200

    Wrote script to query username from database on signup_model.inc.php with safety parameters.

--- includes/signup_model.inc.php
+++ includes/signup_model.inc.php
@@ -1,3 +1,12 @@
 <?php
 
 declare(strict_types=1);
+
+function get_username(object $pdo, string $username) {
+    $query = "SELECT username FROM users WHERE username = :$username;";
+    $stmt = $pdo -> prepare($query);
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+
+
+}
