d96913fbd4a7a3833bbd9d53659f4409b1e7cae0
DeenoDev <deyanoklaasen@outlook.com>
2023-08-20 17:10:39 +0200

    Fixed config_session.php bug.

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -13,15 +13,14 @@ session_set_cookie_params([
 
 session_start();
 
-if (isset($_SESSION["last_regeneration"])) {
+if (!isset($_SESSION["last_regeneration"])) {
     regenerate_session_id();
 }
 else {
     $interval = 60 * 30;
-    if(time() - $_SESSION['last_regeneration'] >= $interval){
+    if(time() - $_SESSION["last_regeneration"] >= $interval){
         session_regenerate_id();
-        $_SESSION["last_regeneration"] = time();
-
+        // $_SESSION["last_regeneration"] = time();
     }
 }
 
