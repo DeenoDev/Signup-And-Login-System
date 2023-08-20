82a996e927434b58a32bcca13f8ce0149077ec49
DeenoDev <deyanoklaasen@outlook.com>
2023-08-20 17:45:56 +0200

    Created persisitent email input field with session variable.

--- includes/signup_view.inc.php
+++ includes/signup_view.inc.php
@@ -3,14 +3,26 @@
 declare(strict_types=1);
 
 function signup_inputs() {
-        <input type="text" name="username" placeholder="Username" class="mb-3">
-        <input type="password" name="pwd" placeholder="Password" class="mb-3">
-        <input type="text" name="email" placeholder="E-mail" class="mb-3">
+        // <input type="text" name="username" placeholder="Username" class="mb-3">
+        // <input type="password" name="pwd" placeholder="Password" class="mb-3">
+        // <input type="text" name="email" placeholder="E-mail" class="mb-3">
 
         if(isset($_SESSION["signup_data"]["username"]) && !isset($_SESSION["errors_signup"]["username_taken"])){
-            echo '<input type="text" name="username" placeholder="Username" class="mb-3">';
+            echo '<input type="text" name="username" placeholder="Username" value="' . $_SESSION["signup_data"]["username"] . '" class="mb-3">';
 
+        } else {
+            echo '<input type="text" name="username" placeholder="Username"  class="mb-3">';
         }
+
+        echo '<input type="password" name="pwd" placeholder="Password" class="mb-3">';
+
+        if(isset($_SESSION["signup_data"]["email"]) && !isset($_SESSION["errors_signup"]["email_used"]) && !isset($_SESSION["errors_signup"]["invalid_email"])){
+            echo '<input type="text" name="email" placeholder="E-mail" value="' . $_SESSION["signup_data"]["email"] . '" class="mb-3">';
+
+        } else {
+            echo '<input type="text" name="email" placeholder="E-mail" class="mb-3">';
+        }
+
 }
 
 function check_signup_errors()
