1075ab28f62001ea0be63bc36db2a9a7d3c3218d
DeenoDev <deyanoklaasen@outlook.com>
2023-08-21 22:10:48 +0200

    Created Try Catch function for login.php.

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -3,4 +3,13 @@
 if($_SERVER["REQUEST_METHOD"] === "POST"){
     $username = $_POST["username"];
     $pwd = $_POST["pwd"];
+
+    try{
+
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+}
+else {
+    header("Location: ../index.php");
+    die();
 }
