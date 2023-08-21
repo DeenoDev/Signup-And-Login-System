e7bcb23beef06f8ff329144b31446fcbddf5a775
DeenoDev <deyanoklaasen@outlook.com>
2023-08-21 22:50:54 +0200

    Added error handlers to login.inc.php file.

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -9,6 +9,42 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
         require_once 'login_model.inc.php';
         require_once 'login_contr.inc.php';
 
+         //ERROR HANDLERS
+         $errors = [];
+
+         if (is_input_empty($username, $pwd, $email)) {
+             $errors["empty_input"] = "Fill in all fields!";
+ 
+         }
+         if (is_email_invalid($email)) {
+             $errors["invalid_email"] = "Invalid email used!";
+ 
+         }
+         if (is_username_taken($pdo, $username)) {
+             $errors["username_taken"] = "Username already taken!";
+ 
+         }
+         if (is_email_registered($pdo, $email)) {
+             $errors["email_used"] = "Email already registered!";
+ 
+         }
+ 
+         require_once 'config_session.inc.php';
+ 
+         if($errors){
+             $_SESSION["errors_signup"] = $errors;
+ 
+             $signupData = [
+                 "username" => $username,
+                 "email" => $email
+             ];
+ 
+             $_SESSION["signup_data"] = $signupData;
+ 
+             header("Location: ../index.php");
+             die();
+         }
+
 
     } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
