91c13cd199045f50aa77c608f2dca00d1eb844a0
DeenoDev <deyanoklaasen@outlook.com>
2023-08-22 22:02:53 +0200

    check_login_errors function completed within login_view.inc.php.

--- includes/login_view.inc.php
+++ includes/login_view.inc.php
@@ -7,11 +7,20 @@ check_login_errors()
     if(isset($_SESSION["errors_login"])){
         $errors = $_SESSION["errors_login"];
 
-        echo "<br>"
-        
+        echo "<br>";
+
+        foreach ($errors as $error){
+            echo '<p class="">' . $error . '</p>';
+
+        }
 
         unset($_SESSION["errors_login"]);
 
+    } else if (isset($_GET['login']) && $_GET['login'] === "success"){
+
+      echo '<br>';
+      echo '<p class="">Login success!</p>';
     }
 
-}
+ }
+
