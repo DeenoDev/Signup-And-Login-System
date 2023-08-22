0c821d1d2ceb8e352111b36be965439a35fa9468
DeenoDev <deyanoklaasen@outlook.com>
2023-08-22 21:19:21 +0200

    regenerate_session_id_loggedin function completed.

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -41,6 +41,12 @@ if(isset($_SESSION['user_id'])){
 function regenerate_session_id_loggedin() 
 {
         session_regenerate_id(true);
+
+        $userId = $_SESSION["user_id"];
+        $newSessionId = session_create_id();
+        $sessionId = $newSessionId . "_" . $userId;
+        session_id($sessionId);
+
         $_SESSION["last_regeneration"] = time();
 
 }
