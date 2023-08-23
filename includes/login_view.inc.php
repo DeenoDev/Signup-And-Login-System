81ec257203e394af8685eddc1a033ec370a66d2b
DeenoDev <deyanoklaasen@outlook.com>
2023-08-23 22:45:04 +0200

    Created output_username function that displays name of logged in user.

--- includes/login_view.inc.php
+++ includes/login_view.inc.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+function output_username() {
+    if(isset($_SESSION["user_id"])){
+        echo "You are logged in as " . $_SESSION["user_username"];
+    } else {
+        echo "You are not logged in";
+    }
+}
+
 function check_login_errors()
 {
     if (isset($_SESSION["errors_login"])) {
