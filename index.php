02f213ca98297b93ca1ee4066c78021bdc25f468
DeenoDev <deyanoklaasen@outlook.com>
2023-08-24 09:23:50 +0200

    Removed current style.css sheet.

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ require_once 'includes/login_view.inc.php';
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="style.css"> 
+     
 </head>
 <body class="position-relative">
 
